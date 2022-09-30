abb69d457588aba03a98ce5865c6fff746a02f60
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-09-30 19:14:37 -0300

    feat: ensure MediatorHandler implements IMediatorHandler

---
+++ src/IFCE.AutoGate.Infrastructure/Gateways/MediatorHandler.cs
@@ -0,0 +1,21 @@
+using IFCE.AutoGate.Core.Contracts;
+using IFCE.AutoGate.Core.Messages;
+using IFCE.AutoGate.Domain.Contracts.Gateways;
+using MediatR;
+
+namespace IFCE.AutoGate.Infrastructure.Gateways;
+
+public class MediatorHandler : IMediatorHandler
+{
+    private readonly IMediator _mediator;
+
+    public MediatorHandler(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<IResult> SendCommand(Command command)
+    {
+        return await _mediator.Send(command);
+    }
+}
