5ae14a56019b0539097f4accf8d4212b2a7e2175
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-09-30 19:01:02 -0300

    feat: improve administrator entity to forget password

--- src/IFCE.AutoGate.Domain/Entities/Administrator.cs
+++ src/IFCE.AutoGate.Domain/Entities/Administrator.cs
@@ -15,4 +15,11 @@ public class Administrator : Entity
     public string? Password { get; private set; }
     public Guid? RecoveryPasswordCode { get; private set; }
     public DateTime? RecoveryPasswordExpiresAt { get; private set; }
+
+    public void ForgetPassword()
+    {
+        RecoveryPasswordCode = new Guid();
+        var expiresAt = DateTime.Now.AddHours(2);
+        RecoveryPasswordExpiresAt = expiresAt;
+    }
 }
