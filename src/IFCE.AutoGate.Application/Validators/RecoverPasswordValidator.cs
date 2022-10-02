e23d6ea45827349da871302a724764a403d9813d
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-10-02 19:49:50 -0300

    feat: add RecoverPasswordValidator

---
+++ src/IFCE.AutoGate.Application/Validators/RecoverPasswordValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+using IFCE.AutoGate.Application.Commands;
+
+namespace IFCE.AutoGate.Application.Validators;
+
+public class RecoverPasswordValidator : AbstractValidator<RecoverPasswordCommand>
+{
+    public RecoverPasswordValidator()
+    {
+        RuleFor(c => c.Email)
+            .NotEmpty().WithMessage("O e-mail é obrigatório")
+            .EmailAddress().WithMessage("O e-mail informado é inválido");
+    }
+}
