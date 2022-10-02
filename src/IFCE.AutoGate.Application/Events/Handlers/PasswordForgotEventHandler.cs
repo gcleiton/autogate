53b13431d742eafa4c81d0ed1d931b223ed30237
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-10-02 19:49:41 -0300

    feat: add handler to PasswordForgotEvent

---
+++ src/IFCE.AutoGate.Application/Events/Handlers/PasswordForgotEventHandler.cs
@@ -0,0 +1,27 @@
+using IFCE.AutoGate.Domain.Contracts.Gateways;
+using IFCE.AutoGate.Domain.ValueObjects;
+using MediatR;
+
+namespace IFCE.AutoGate.Application.Events.Handlers;
+
+public class PasswordForgotEventHandler : INotificationHandler<PasswordForgotEvent>
+{
+    private readonly IMailSender _mailSender;
+
+    public PasswordForgotEventHandler(IMailSender mailSender)
+    {
+        _mailSender = mailSender;
+    }
+
+    public async Task Handle(PasswordForgotEvent message, CancellationToken cancellationToken)
+    {
+        var mail = new MailMessage
+        {
+            To = message.Email,
+            Subject = "AutoGate | Recuperação de Senha",
+            Body = $"Olá {message.Name} seu código de recuperação é {message.RecoveryPasswordCode}"
+        };
+
+        await _mailSender.SendEmail(mail);
+    }
+}
