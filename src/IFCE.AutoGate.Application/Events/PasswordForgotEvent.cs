9609c80af1adb832d64d8a6c1e48d0b6e2978fdb
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-10-02 19:48:47 -0300

    feat: add handler to RecoverPasswordCommand

---
+++ src/IFCE.AutoGate.Application/Events/PasswordForgotEvent.cs
@@ -0,0 +1,19 @@
+using IFCE.AutoGate.Core.Messages;
+
+namespace IFCE.AutoGate.Application.Events;
+
+public class PasswordForgotEvent : Event
+{
+    public PasswordForgotEvent(string name, string email, Guid recoveryPasswordCode)
+    {
+        Name = name;
+        Email = email;
+        if (recoveryPasswordCode == Guid.Empty)
+            throw new InvalidOperationException("O código de recuperação da senha é inválido");
+        RecoveryPasswordCode = recoveryPasswordCode;
+    }
+
+    public string Name { get; }
+    public string Email { get; }
+    public Guid RecoveryPasswordCode { get; }
+}
