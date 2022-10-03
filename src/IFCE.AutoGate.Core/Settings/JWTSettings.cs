aa011733d55d6a950b9b2b58b30b8c1aa61b37f0
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-10-03 18:15:36 -0300

    feat: implements ITokenHandler to use jwt

---
+++ src/IFCE.AutoGate.Core/Settings/JWTSettings.cs
@@ -0,0 +1,9 @@
+namespace IFCE.AutoGate.Core.Settings;
+
+public class JWTSettings
+{
+    public string Key { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public int ExpirationInMs { get; set; }
+}
