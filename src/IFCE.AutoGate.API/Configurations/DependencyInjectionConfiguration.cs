e7e40c3bf830c2e0caa4fca54c159746d7b003d9
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-10-03 18:22:47 -0300

    feat: register new dependencies

--- src/IFCE.AutoGate.API/Configurations/DependencyInjectionConfiguration.cs
+++ src/IFCE.AutoGate.API/Configurations/DependencyInjectionConfiguration.cs
@@ -1,10 +1,12 @@
 using FluentValidation;
 using IFCE.AutoGate.Application;
+using IFCE.AutoGate.Core.Communication;
 using IFCE.AutoGate.Domain.Contracts.Gateways;
 using IFCE.AutoGate.Domain.Contracts.Repositories;
 using IFCE.AutoGate.Infrastructure.Gateways;
 using IFCE.AutoGate.Infrastructure.Repositories;
 using MediatR;
+using INotification = IFCE.AutoGate.Core.Contracts.INotification;
 
 namespace IFCE.AutoGate.API.Configurations;
 
@@ -19,5 +21,7 @@ public static class DependencyInjectionConfiguration
         services.AddScoped<IAdministratorRepository, AdministratorRepository>();
         services.AddScoped<IMailSender, MailKitMailSender>();
         services.AddScoped<IHasher>(s => new BcryptHasher(12));
+        services.AddScoped<ITokenHandler, JwtTokenHandler>();
+        services.AddScoped<INotification, Notification>();
     }
 }
