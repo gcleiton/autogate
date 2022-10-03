146412ff307ba7d6c56f793939cee6a627f0c60c
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-10-03 18:21:27 -0300

    feat: improve IMediatorHandler to send request

--- src/IFCE.AutoGate.Domain/Contracts/Gateways/IMediatorHandler.cs
+++ src/IFCE.AutoGate.Domain/Contracts/Gateways/IMediatorHandler.cs
@@ -5,6 +5,7 @@ namespace IFCE.AutoGate.Domain.Contracts.Gateways;
 
 public interface IMediatorHandler
 {
+    Task<TResponse> SendRequest<TResponse>(Request<TResponse> request);
     Task<IResult> SendCommand(Command command);
     Task PublishEvent(Event message);
 }
