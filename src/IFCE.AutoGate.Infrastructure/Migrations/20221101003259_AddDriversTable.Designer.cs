ab00bfd6ba77530b33fab790b804a3240b7fb85e
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-11-01 08:30:07 -0300

    feat: add migration

--- src/IFCE.AutoGate.Infrastructure/Migrations/20221101003259_AddDriversTable.Designer.cs
+++ src/IFCE.AutoGate.Infrastructure/Migrations/20221101003259_AddDriversTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IFCE.AutoGate.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace IFCE.AutoGate.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221101003259_AddDriversTable")]
+    partial class AddDriversTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/IFCE.AutoGate.Infrastructure/Migrations/20221101003259_AddDriversTable.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IFCE.AutoGate.Infrastructure.Migrations
+{
+    public partial class AddDriversTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "drivers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false),
+                    PhotoUrl = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false),
+                    BornAt = table.Column<DateTime>(type: "TIMESTAMP", nullable: false),
+                    Phone = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    License = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    Tag = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "NOW()"),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "NOW()"),
+                    ModifiedBy = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_drivers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicle_categories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehicle_categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Plate = table.Column<string>(type: "character varying(7)", maxLength: 7, nullable: false),
+                    Model = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false),
+                    CategoryId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DriverId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "NOW()"),
+                    CreatedBy = table.Column<int>(type: "integer", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "TIMESTAMP", nullable: false, defaultValueSql: "NOW()"),
+                    ModifiedBy = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehicles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_vehicles_drivers_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "drivers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_vehicles_vehicle_categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "vehicle_categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_drivers_Email_Tag",
+                table: "drivers",
+                columns: new[] { "Email", "Tag" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_vehicles_CategoryId",
+                table: "vehicles",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_vehicles_DriverId",
+                table: "vehicles",
+                column: "DriverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_vehicles_Plate",
+                table: "vehicles",
+                column: "Plate",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "vehicles");
+
+            migrationBuilder.DropTable(
+                name: "drivers");
+
+            migrationBuilder.DropTable(
+                name: "vehicle_categories");
+        }
+    }
+}
