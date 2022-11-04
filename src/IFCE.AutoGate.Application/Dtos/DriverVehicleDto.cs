1587a0f5a5861ebfd00b652d45105d03cfced475
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-11-04 20:12:54 -0300

    feat: add CreateDriverCommand

---
+++ src/IFCE.AutoGate.Application/Dtos/DriverVehicleDto.cs
@@ -0,0 +1,8 @@
+namespace IFCE.AutoGate.Application.Dtos;
+
+public class DriverVehicleDto
+{
+    public string Plate { get; set; }
+    public string Model { get; set; }
+    public Guid CategoryId { get; set; }
+}
---
+++ src/IFCE.AutoGate.Application/UseCases/CreateDriver/CreateDriverCommand.cs
@@ -0,0 +1,18 @@
+using IFCE.AutoGate.Application.Dtos;
+using IFCE.AutoGate.Core.Messages;
+using Microsoft.AspNetCore.Http;
+
+namespace IFCE.AutoGate.Application.UseCases.CreateDriver;
+
+public class CreateDriverCommand : Command<bool>
+{
+    public string Name { get; set; }
+    public IFormFile? Photo { get; set; }
+    public string Email { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public string Phone { get; set; }
+    public string License { get; set; }
+    public string CardNumber { get; set; }
+
+    public IEnumerable<DriverVehicleDto> Vehicles { get; set; }
+}
