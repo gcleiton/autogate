60f908704e741aa4b29e1cbbbf50fe5d0ead46d5
Gabriel Cleiton <gcleiton.dev@gmail.com>
2022-11-12 10:22:42 -0300

    feat: add UpdateDriverCommand

---
+++ src/IFCE.AutoGate.Application/UseCases/UpdateDriver/UpdateDriverCommand.cs
@@ -0,0 +1,19 @@
+using IFCE.AutoGate.Application.Dtos;
+using IFCE.AutoGate.Core.Messages;
+using Microsoft.AspNetCore.Http;
+
+namespace IFCE.AutoGate.Application.UseCases.UpdateDriver;
+
+public class UpdateDriverCommand : Command<bool>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public IFormFile? Photo { get; set; }
+    public string Email { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public string Phone { get; set; }
+    public string License { get; set; }
+    public string CardNumber { get; set; }
+
+    public IEnumerable<DriverVehicleDto> Vehicles { get; set; }
+}
